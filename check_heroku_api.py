c59ab7f7cc35ca79d6e0dd1577a8b7bde9395dd0
Ananda Dwi Putri <anandadwiputri078@gmail.com>
2021-11-12 13:03:59 +0700

    edit check file

--- check_heroku_api.py
+++ check_heroku_api.py
@@ -16,13 +16,7 @@ data = {
     "hoursPerWeek": 60,
     "nativeCountry": "United-States"
     }
-<<<<<<< HEAD
 r = requests.post('https://project3-nd.herokuapp.com/', json=data)
-||||||| 3c1040e
-r = requests.post('https://ml-heroku-fastapi.herokuapp.com/', json=data)
-=======
-r = requests.post('https://test-project3.herokuapp.com/', json=data)
->>>>>>> 3a86b1ae526dd9430f116f578257c228b040cc21
 
 assert r.status_code == 200
 
